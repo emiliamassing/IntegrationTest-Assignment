815fbce4cbfb3369585a0c07564d8922c4c91a7e
emiliamassing <emiliamassings@gmail.com>
2023-01-31 20:05:50 +0100

    ✨Added coverage, 📝Test for movieSort

--- src/__test__/functions.test.ts
+++ src/__test__/functions.test.ts
@@ -5,17 +5,22 @@ import { IMovie } from "../ts/models/Movie";
 import { movieSort } from "../ts/functions";
 
 describe('Tests for movieSort', () => {
-    document.body.innerHTML= `
-        <div id="movie-container"></div>
-    `;
 
-    let movieList: IMovie[] = [
-        {Title: 'Empire strikes back', imdbID: 'aaa', Type: 'Movie', Poster:'picture', Year:'1980' },
-        {Title: 'Return of the jedi', imdbID: 'bbb', Type: 'Movie', Poster:'picture', Year:'1983' },
-        {Title: 'Mandalorian', imdbID: 'ccc', Type: 'Series', Poster:'picture', Year:'2019' }
-    ];
+    test('Should sort movies if list isnt empty', () => {
+        document.body.innerHTML= `
+            <div id="movie-container"></div>
+        `;
 
-    movieSort(movieList);
+        let movieList: IMovie[] = [
+            {Title: 'Empire Strikes Back', imdbID: 'aaa', Type: 'Movie', Poster: 'picture', Year: '1980' },
+            {Title: 'Return Of The Jedi', imdbID: 'bbb', Type: 'Movie', Poster: 'picture', Year: '1983' },
+            {Title: 'Mandalorian', imdbID: 'ccc', Type: 'Series', Poster: 'picture', Year: '2019' }
+        ];
 
-    expect(movieList[0].Title).toEqual('Empire strikes back');
+        movieSort(movieList);
+
+        expect(movieList[0].Title).toEqual('Empire Strikes Back');
+        expect(movieList[1].Title).toEqual('Mandalorian');
+        expect(movieList[2].Title).toEqual('Return Of The Jedi');
+    });
 });
