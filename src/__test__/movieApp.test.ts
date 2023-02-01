34edf5058e9e7ffb9741b45152299c22487cb777
emiliamassing <emiliamassings@gmail.com>
2023-02-01 17:12:21 +0100

    added spy test for init

--- src/__test__/movieApp.test.ts
+++ src/__test__/movieApp.test.ts
@@ -1,1 +1,25 @@
+/*** 
+* @jest-environment jsdom
+*/
 import * as movieApp from "../ts/movieApp";
+
+describe('Tests for init', () => {
+    test('Should spy on handleSubmit()', () =>{
+        document.body.innerHTML = `
+        <form id="searchForm">
+            <input type="text" id="searchText" placeholder="Skriv titel här" />
+            <button type="submit" id="search">Sök</button>
+        </form>
+        `;
+
+        let spyOnHandleSubmit = jest.spyOn(movieApp, 'handleSubmit').mockReturnValue(new Promise<void>((resolve) => {
+            resolve();
+        }));
+
+        movieApp.init();
+
+        (document.querySelector('#searchForm') as HTMLFormElement)?.submit();
+        expect(spyOnHandleSubmit).toHaveBeenCalledTimes(1);
+        spyOnHandleSubmit.mockRestore();
+    });
+});
