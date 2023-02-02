8305b8b069794a48ca238da221d3c442dc5ed06d
emiliamassing <emiliamassings@gmail.com>
2023-02-02 12:38:52 +0100

    📝adjusted settings for package.json, ⚡️Added test for createHtml

--- src/__test__/movieApp.test.ts
+++ src/__test__/movieApp.test.ts
@@ -1,6 +1,7 @@
 /*** 
 * @jest-environment jsdom
 */
+import { IMovie } from "../ts/models/Movie";
 import * as movieApp from "../ts/movieApp";
 
 describe('Tests for init', () => {
@@ -21,6 +22,44 @@ describe('Tests for init', () => {
         (document.querySelector('#searchForm') as HTMLFormElement)?.submit();
         expect(spyOnHandleSubmit).toHaveBeenCalledTimes(1);
         spyOnHandleSubmit.mockRestore();
+    });
+});
+
+describe('Tests for createHtml', () => {
+    test('Should create elements and display movies', () => {
+        document.body.innerHTML = `
+            <div id="movie-container"></div>
+        `;
+
+        const container = document.querySelector('#movie-container') as HTMLDivElement;
+
+        let movieList: IMovie[] = [
+            {
+                Title: 'The Phantom Menace',
+                imdbID: 'tt0120915',
+                Type: 'movie', 
+                Poster: 'picture', 
+                Year: '1999' 
+            },
+            {   Title: 'Attack Of The Clones',
+                imdbID: 'tt0121765', 
+                Type: 'movie',
+                Poster: 'picture', 
+                Year: '2003'
+            },
+            {   Title: 'Revenge Of The Sith',
+                imdbID: 'tt0121766', 
+                Type: 'movie',
+                Poster: 'picture',
+                Year: '2005' 
+            },
+        ];
+
+        movieApp.createHtml(movieList, container);
+        
+        expect(movieList[0].Title).toEqual('The Phantom Menace');
+        expect(movieList[1].Title).toEqual('Attack Of The Clones');
+        expect(movieList[2].Title).toEqual('Revenge Of The Sith');
     });
 });
 
